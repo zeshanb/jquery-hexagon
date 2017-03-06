a3e95e06c84c2e458590d94fb33be15d3f361434
Zeshan B <zeshanb@zdatatech.com>
2017-03-06 14:32:33 -0500

    eslint issues

--- js/hexagon.js
+++ js/hexagon.js
@@ -2,105 +2,96 @@
 //Zeshan B.
 //11-7-2016
 
+(function(){
+
 	//x by x
+	var starChars;
 	var enteredNum;
-	var oddNum;
-	var starChars = "";
-	var spaceBefore = "";
 	var numberOfSpacesFirstLine;
-	var even = function(x){ //A function to determine if a number is even
+	var even = function(x){	//A function to determine if a number is even
 		return x % 2 === 0;
 	};
 	
 	var lineSize = function(){
 		
-		starChars  = "";
+		starChars  = '';
 	
 		for(var i=0; i < enteredNum; i++){
 			
-			starChars += "*";
+			starChars += '*';
 
 		}
-	}
 	
-	var lineSpace = function(){
-	
-		spaceBefore = "";
-		
-		for(var j=0; j < numberOfSpacesFirstLine; j++){
-		
-			spaceBefore += '\u00A0';
-		
-		}
-	
-	}
+	};
 	
 	var makeShape = function(){
 	
-	        var numOfSpaces = numberOfSpacesFirstLine;
+		var numOfSpaces = numberOfSpacesFirstLine;
 			
-			var descSpace = 0;
+		var descSpace = 0;
 	
-	        lineSize();
+		lineSize();
 			
-			for(var i=0; i <= numberOfSpacesFirstLine; i++){
+		for(var i=0; i <= numberOfSpacesFirstLine; i++){
 			
-				for(var j=0; j <= numberOfSpacesFirstLine; j++){
+			for(var j=0; j <= numberOfSpacesFirstLine; j++){
 				
-					$(".shape").append("\u00A0");
+				$('.shape').append('\u00A0');
 				
-				}
-
-				$(".shape").append(""+ starChars +"");
-
-				$(".shape").append("</br>");
-				
-				starChars += "*";
-				
-				numberOfSpacesFirstLine--;
-			
-			}
-			
-			for(numberOfSpacesFirstLine;numberOfSpacesFirstLine <= numOfSpaces; numberOfSpacesFirstLine++){
-
-				for(var k=0; k <= numberOfSpacesFirstLine; k++){
-				
-					$(".shape").append("\u00A0");
-				
-				}
-
-				$(".shape").append(""+ starChars.substr(descSpace) +"");
-
-				$(".shape").append("</br>");
-				
-				descSpace++;
-			
 			}
 
-	}
+			$('.shape').append(''+ starChars +'');
 
-	
-	$(".NumUpdate").click(function(){
-	
-		//clear shape
-		$(".shape").text(" ");
-		enteredNum = $("input[name$='enteredNum']").val();
+			$('.shape').append('</br>');
+				
+			starChars += '*';
+				
+			numberOfSpacesFirstLine--;
+
+		}
 		
-		switch(even(enteredNum)){
-		
-			case true:
-				numberOfSpacesFirstLine = enteredNum / 2;
-			break;
-			case false:
-				numberOfSpacesFirstLine = Math.round((Math.floor(enteredNum) + 2) / 2);
-			break;
+		for(numberOfSpacesFirstLine;
+		numberOfSpacesFirstLine <= numOfSpaces; 
+		numberOfSpacesFirstLine++)
+		{
+
+			for(var k=0; k <= numberOfSpacesFirstLine; k++){
+
+				$('.shape').append('\u00A0');
+				
+			}
+			
+			$('.shape').append(''+ starChars.substr(descSpace) +'');
+			
+			$('.shape').append('</br>');
+			
+			descSpace++;
 			
 		}
 		
-		$(".notice").text("You Entered "+ enteredNum +" x "+ enteredNum +", Spaces equals " + numberOfSpacesFirstLine+ "");
+	};
+
+	$('.NumUpdate').click(function()
+	{
+		//clear printed shape
+		$('.shape').text(' ');
+		enteredNum = $('input[name$=\'enteredNum\']').val();
+		
+		switch(even(enteredNum)){
+		
+		case true:
+			numberOfSpacesFirstLine = enteredNum / 2;
+			break;
+		case false:
+			numberOfSpacesFirstLine = Math.round((Math.floor(enteredNum) + 2) / 2);
+			break;
+		
+		}
+		
+		$('.notice').text('You Entered '+ enteredNum +' x '+ enteredNum +', Spaces equals ' + numberOfSpacesFirstLine+ '');
 
 		makeShape();
 		
 	});
 	
-	
+})();
